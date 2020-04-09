df91e4b0e43b1c25d54d16f7c1e3b783f7fe7b0d
kodamayuto <yuto2001gyokugan@gmail.com>
2020-04-09 11:46:14 +0900

    ok

---
+++ PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController playerController;
+    [SerializeField]
+    private float speed = 12f;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float gravity = 9.81f;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundDistance = 1.0f;
+    [SerializeField]
+    private LayerMask groundMask;
+    #region Flag
+    private bool isGrounded;
+    #endregion //!Flag
+    private float x;
+    private float z;
+    private Vector3 move;
+    private Vector3 velocity;
+    //基本的に関数内で変数宣言は行うな
+    //パフォーマンス低下につながる
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        move = transform.right * x + transform.forward * z;
+        playerController.Move(move * speed * Time.deltaTime);
+        FuncAnimator();
+
+        FuncGravity();
+    }
+    void FuncAnimator()
+    {
+        if (move.magnitude > 0.2f)
+        {
+            animator.SetFloat("Speed", move.magnitude);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+    void FuncGravity()
+    {
+        //Apply gravity
+        velocity.y = velocity.y - (gravity * Time.deltaTime);
+
+        playerController.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ PlayerMovement2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement2 : MonoBehaviour
+{
+    #region SerializeField
+    [SerializeField]
+    private CharacterController playerController;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private Animator animator;
+    #endregion //!SerializeField
+
+
+    private float x;
+    private float z;
+
+    private Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FuncMove();
+        FuncAnimator();
+        FuncGravity();
+    }
+
+    void FuncMove()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        move = transform.right * x + transform.forward * z;
+        playerController.Move(move * speed * Time.deltaTime);
+    }
+    void FuncAnimator()
+    {
+        if (move.magnitude > 0.1f)
+        {
+            animator.SetFloat("Speed", move.magnitude);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+    void FuncGravity()
+    {
+        
+    }
+}
